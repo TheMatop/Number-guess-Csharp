b5dc05f13f5acc41f445fa5831111f3cd85e1d58
TheMatop <matopmatanhillel108a1@gmail.com>
2025-06-13 12:20:42 +0300

    Added expectition

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,24 +6,40 @@ int answer = rnd.Next(1, 11);
 while (true)
 {
     Console.Write("Enter a randon number between 1-10: ");
-    String give = Console.ReadLine();
-    int final = int.Parse(give);
-    if (final == answer)
+    try
     {
-        Console.WriteLine("You are correct!");
-        break;
-    } else {
-        if (final > answer)
+        String give = Console.ReadLine();
+        int final = int.Parse(give);
+
+        if (final == answer)
         {
-            Console.WriteLine("Smaller");
-            continue;
+            Console.WriteLine("You are correct!");
+            break;
         }
-        else if (final < answer)
+        else
         {
+            if (final > answer)
             {
-                Console.WriteLine("Bigger");
+                Console.WriteLine("Smaller");
                 continue;
             }
+            else if (final < answer)
+            {
+                {
+                    Console.WriteLine("Bigger");
+                    continue;
+                }
+            }
         }
+    }
+    catch (System.FormatException ex)
+    {
+        Console.WriteLine("Put a number!");
+        continue;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex);
+        continue;
     }
 }
