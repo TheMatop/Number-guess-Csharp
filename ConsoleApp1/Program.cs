a2b94615d084033b2ea973490c03efb307db4cc6
TheMatop <matopmatanhillel108a1@gmail.com>
2025-06-13 12:04:35 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Random rnd = new Random();
+int answer = rnd.Next(1, 11);
+
+while (true)
+{
+    Console.Write("Enter a randon number between 1-10: ");
+    String give = Console.ReadLine();
+    int final = int.Parse(give);
+    if (final == answer)
+    {
+        Console.WriteLine("You are correct!");
+        break;
+    } else {
+        if (final > answer)
+        {
+            Console.WriteLine("Smaller");
+            continue;
+        }
+        else if (final < answer)
+        {
+            {
+                Console.WriteLine("Bigger");
+                continue;
+            }
+        }
+    }
+}
